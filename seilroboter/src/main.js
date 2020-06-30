8866b65ba9f27d71cc139e793bacd3e114b9fcea
taramonheim <taramonheim@me.com>
2020-06-30 19:41:58 +0200

    integrated socket.io

--- seilroboter/src/main.js
+++ seilroboter/src/main.js
@@ -3,9 +3,43 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from "socket.io-client"
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://3.121.41.235:3000/',
+  vuex: {
+      store,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_'
+  },
+  options: { path: "/my-app/" } //Optional options
+}))
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// this.$socket.emit('nachrichtenname', data)
+
+/*im script-Teil der Komponente
+export default {
+  name: 'Component',
+  props: {
+    test: {
+      type: String,
+    }
+  },
+  sockets: {
+        connect: function () {
+            console.log('socket connected')
+        },
+        nachrichtenname: function (data) {
+            console.log('this method was fired by the socket server. eg: io.emit("nachrichtenname", data)')
+        }
+  } 
+  //hier folgen ggf. noch methods, mounted() usw.
+}*/
