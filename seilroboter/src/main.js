a80e601ed811a822a76dbf6a5f5b6904bfeb8f9f
taramonheim <taramonheim@me.com>
2020-07-02 10:26:50 +0200

    update

--- seilroboter/src/main.js
+++ seilroboter/src/main.js
@@ -23,6 +23,32 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
+/* 
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import store from './store'
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://3.121.41.235:3000',
+  vuex: {
+      store,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_'
+  },
+  options: { path: "/my-app/" } //Optional options
+}))
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')*/ 
+
 // this.$socket.emit('nachrichtenname', data)
 
 /*im script-Teil der Komponente
